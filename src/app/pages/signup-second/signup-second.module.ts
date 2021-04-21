8d06e3ec193272a1d470e3f3cc8fcc0088ce3249
topcoder917 <snowgirl1991718@hotmail.com>
2021-04-21 19:26:07 +0800

    2021/04/21

--- src/app/pages/signup-second/signup-second.module.ts
+++ src/app/pages/signup-second/signup-second.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SignupSecondPageRoutingModule } from './signup-second-routing.module';
 
 import { SignupSecondPage } from './signup-second.page';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     SignupSecondPageRoutingModule
   ],
--- src/app/pages/signup-second/signup-second.page.ts
+++ src/app/pages/signup-second/signup-second.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { CartSettingService } from "../../services/global-carttsetting/cart-setting.service";
 
 @Component({
   selector: 'app-signup-second',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-second.page.scss'],
 })
 export class SignupSecondPage implements OnInit {
+  validationsform: FormGroup;
+  countryList = [];
+  stateList = [];
+  isDropBoxForState = false;
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+    public cartSettingService: CartSettingService,
+  ) { }
 
   ngOnInit() {
+    this.validationsform = this.formBuilder.group({
+      first_name: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      last_name: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      address1: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      address2: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      city: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      post_code: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      state: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      countrykey: new FormControl('', Validators.compose([
+        Validators.required
+      ]))         
+    });
   }
-
+  async ionViewWillEnter(){
+    this.countryList = this.cartSettingService.countryList;
+    this.stateList = this.cartSettingService.states_au;
+  }
+  changeCountry(e){
+    var countryVal = e.detail.value;
+    if(countryVal == 463)
+      this.isDropBoxForState = true;
+    else
+      this.isDropBoxForState = false;
+  }
 }
